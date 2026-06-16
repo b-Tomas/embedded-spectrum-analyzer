e74fa554051a4b7d4799c04ec0d921757097b42b
Tomás Badenes <41840767+b-Tomas@users.noreply.github.com>
2026-06-16 00:48:15 -0300

    Mode switching (#25)

--- firmware/src/spectrum-analyzer.c
+++ firmware/src/spectrum-analyzer.c
@@ -1,4 +1,5 @@
 #include "LPC17xx.h"
+#include "display/display.h"
 #include "display/i2c_bus.h"
 #include "gpdma/gpdma.h"
 #include "input/keyboard.h"
@@ -26,17 +27,38 @@ int main(void) {
     // ReSharper disable once CppDFAEndlessLoop
     while (true) {
         __WFI();
-
         // Process keyboard input if any
         char c;
         while (kbd_pop(&c)) {
             printf("pressed key %c\n", c);
-            switch (SYSTEM.mode) {
-            case equalizerMode:
-                system_processKeyEqualizerMode(c);
-                break;
-            default:
-                break;
+            if (c == 'A' || c == 'B' || c == 'C') {
+                // Change mode if applicable
+                SYSTEM.flag_ModeConfigured = RESET;
+                switch (c) {
+                case 'A':
+                    system_setMode(realTimeMode);
+                    break;
+                case 'B':
+                    system_setMode(noiseSamplingMode);
+                    break;
+                case 'C':
+                    system_setMode(equalizerMode);
+                    break;
+                default:
+                    break;
+                }
+                // HACK: clear the display
+                display_clearCanvas();
+                display_displayCanvas();
+            } else {
+                // The current mode consumes the key
+                switch (SYSTEM.mode) {
+                case equalizerMode:
+                    system_processKeyEqualizerMode(c);
+                    break;
+                default:
+                    break;
+                }
             }
         }
 
